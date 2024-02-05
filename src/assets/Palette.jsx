ce829fe5eb4385097426bcb8b19f318466d837af
Giuliana_Zorzoli <giulijazorzoli@gmail.com>
2024-02-05 17:21:55 -0300

    Added palettes

--- src/assets/Palette.jsx
+++ src/assets/Palette.jsx
@@ -1,6 +1,15 @@
 import React, { useState } from 'react'
 import { useLocation } from 'react-router-dom';
 
+
+const copiarTexto= async (text) =>{
+    try{
+        await navigator.clipboard.writeText(text);
+        console.log("copiado");
+    }catch(err){
+        console.log("error al copiar");
+    }
+  }
 export const Palette = () => {
     const location = useLocation();
     const { color } = location.state
@@ -19,14 +28,36 @@ export const Palette = () => {
                 <div style={{backgroundColor:c4}}></div>
             </div>
             <div className='circles'>
-                <div style={{backgroundColor:c1}}></div><span>{c1}</span>
-                <div style={{backgroundColor:c2}}></div><span>{c2}</span>
-                <div style={{backgroundColor:c3}}></div><span>{c3}</span>
-                <div style={{backgroundColor:c4}}></div><span>{c4}</span>
+                <div className='circle'><div style={{backgroundColor:c1}}></div><span onClick={e => copiarTexto(c1)}>{c1}</span></div>
+                <div className='circle'><div style={{backgroundColor:c2}}></div><span onClick={e =>copiarTexto(c2)}>{c2}</span></div>
+                <div className='circle'><div style={{backgroundColor:c3}}></div><span onClick={e =>copiarTexto(c3)}>{c3}</span></div> 
+                <div className='circle'><div style={{backgroundColor:c4}}></div><span onClick={e =>copiarTexto(c4)}>{c4}</span></div>
             </div>
         </section>
-        <section className='paletteDemo'>
-            <div>
+        <section className='paletteDemoContainer'>
+            <div className='paletteDemo' style={{background : c2}}>
+                <div className='demoNav' style={{background : c1}}>
+                    <h6>ABOUT</h6>
+                    <h6>PRODUCTS</h6>
+                    <h6>CONTACT</h6>
+                </div>
+                <div className='demoMain'>
+                    <div className='demoTitle'>
+                        <h2 style={{color : c3}}>COOL PAGE</h2>
+                        <p style={{color : c1}}>Esta es una página muy cool, donde hay info cool de cosas muy cool. Bienvenido a esta página cool.</p>
+                    </div>
+                    <div className='demoSection' style={{background : c3}}>
+                        <p>Esta sección muestra más info de esta página tan cool, si quieres conocer mas puedes seguir navegando por esta página tan cool.</p>
+                    </div>
+                    <div className='demoButton'>
+                        <button style={{borderColor : c3}} >PAGINA SUPER COOL</button>
+                    </div>
+
+                </div>
+                <div className='demoFooter' style={ {background : c4}}>
+                    <p>CONTACTO DE COOL PAGE</p>
+                    <p>page@coolpage.com</p>
+                </div>
             </div>
         </section>
     </article>
--- src/assets/Palettes.jsx
+++ src/assets/Palettes.jsx
@@ -4,23 +4,23 @@ import { Link } from 'react-router-dom';
 const palettes = [
   {
     id : 1,
-    colors : ['#4be811','#b6e811','#11e888','#11e5e8']
+    colors : ['#324047','#EFEFEF','#00CECE','#00A8A8']
   },
   {
     id : 2,
-    colors : ['#e811e1','#b6e811','#11e888','#11e5e8']
+    colors : ['#0E0741','#FFFFFF','#8236FD','#ABABB9']
   },
   {
     id : 3,
-    colors : ['#4be811','#b6e811','#11e888','#11e5e8']
+    colors : ['#212832','#F2F2F2','#F96C00','#393D46']
   },
   {
     id : 4,
-    colors : ['#4be811','#b6e811','#11e888','#11e5e8']
+    colors : ['#32293F','#F0F7F4','#705D56','#99E1D9']
   },
   {
     id : 5,
-    colors : ['#4be811','#b6e811','#11e888','#11e5e8']
+    colors : ['#132E35','#AFB3B7','#2D4A53','#0D1F23']
   },
   {
     id : 6,
